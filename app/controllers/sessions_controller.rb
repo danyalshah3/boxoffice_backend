fca00536c0c68d5c1dd2dbd8ca55d88f1cb1dad8
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-14 12:24:30 -0500

    added login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+
+
+    def create
+        user = user.find_by_username(params[:id])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else render json: {errors: "User does'nt exist!🤦🏻"}, status: :forbidden
+        end
+    end
 end
