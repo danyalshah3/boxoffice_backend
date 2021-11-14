f6314061baf0b19f2993fc88c7bc1aee1d60eabe
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-14 11:37:35 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "danyal" username: "danyalshah", creditcardnum: Faker::Finance.credit_card, password: "1234567")
+# User.create(name: "danyal" username: "danyalshah", creditcardnum: Faker::Finance.credit_card, password: "1234567")
 
 film_arr = [
   'tt3896198',
