448e9afb3ad9cbe3f2acaaab09fe7ef4cca70791
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-14 12:26:08 -0500

    added custome route for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resources :movies
-
-  post "/login" to: 'session#create'
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
