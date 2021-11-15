0e85998f70b403df92bf091b47878a69283caf04
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-15 15:17:40 -0500

    migrations and validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+    has_many :transations
+    has_many :users, through: :transations
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_160959) do
+ActiveRecord::Schema.define(version: 2021_11_15_200041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2021_11_14_160959) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "transations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_transations_on_movie_id"
+    t.index ["user_id"], name: "index_transations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_11_14_160959) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "transations", "movies"
+  add_foreign_key "transations", "users"
 end
